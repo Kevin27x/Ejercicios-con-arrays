66a1b2087c08b877bf6df0c1633d530b95a11788
kevincollao <kevin.collao1@gmail.com>
2022-01-26 22:03:10 -0300

    Ejercicios Array

---
+++ index.js
@@ -0,0 +1,29 @@
+var autos = ["chevrolet", "mazda", "suzuki"];
+
+var objetosDentroArreglo = [
+    {nombre: "juan"},
+    {edad: "25"}
+]
+var yo = ["kevin", "25/08/94", 27, 300000, false];
+
+var programa_web = [
+    "JavaScript",
+    "React JS",
+    "Vue JS",
+    "Angular JS",
+    {BackEnd: "Node JS"}
+    ];
+
+console.log(autos);
+console.log(objetosDentroArreglo);
+console.log(yo);
+console.log(programa_web[2])+console.log(programa_web[4]);
+
+var arregloDentro = [
+    ["1","2","3"],
+    ["5","8","13"],
+    ["21","34","55"],
+];
+//Arrays multidimensional. Se accede al objeto array de dimensión 2, con un corchete seguido del primero
+console.log(arregloDentro[0][1])
+console.log(arregloDentro.length)
---
+++ index2.js
@@ -0,0 +1,2 @@
+let datosWeb = ["JavaScript","VueJS","AngularJS","ReactJS","NodeJS"];
+console.log(datosWeb[datosWeb.length-1]) /*Último elemento del array*./0000000000000000000000000N-fpfhylg¿0. 
